cfe89fb908c75c41d5df182a3195b6223cbaeaf8
Matveymayner <127684055+Matveymayner@users.noreply.github.com>
2023-05-13 23:05:12 +0300

    eeprom_code.lua

---
+++ eeprom_code.lua
@@ -0,0 +1,33 @@
+local component = require("component")
+local gpu = component.gpu
+
+local width, height = gpu.getResolution()
+local worms = {}
+
+-- Функция для создания червя на случайной позиции
+local function createWorm()
+  local x = math.random(1, width)
+  local y = math.random(1, height)
+  local worm = {x = x, y = y}
+  table.insert(worms, worm)
+end
+
+-- Функция для обновления позиции червей и их отображения
+local function updateWorms()
+  gpu.fill(1, 1, width, height, " ") -- Очищаем экран
+  for _, worm in ipairs(worms) do
+    gpu.set(worm.x, worm.y, "------") -- Отображаем червя
+    worm.x = worm.x + math.random(-1, 1) -- Случайное перемещение по оси X
+    worm.y = worm.y + math.random(-1, 1) -- Случайное перемещение по оси Y
+    -- Ограничиваем позицию червя в пределах экрана
+    worm.x = math.max(1, math.min(worm.x, width))
+    worm.y = math.max(1, math.min(worm.y, height))
+  end
+end
+
+-- Основной цикл программы
+while true do
+  createWorm() -- Создаем новых червей
+  updateWorms() -- Обновляем позиции червей
+  os.sleep(0.1) -- Приостанавливаем выполнение на некоторое время
+end
